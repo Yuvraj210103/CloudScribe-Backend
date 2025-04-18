fccda0f09b07c9092080ae863319edd7ab7cda65
Yuvraj210103 <ys146228@gmail.com>
2025-04-18 11:05:06 +0530

    refactor

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,19 @@ import cors from "cors";
 import api from "./api/index.api";
 import { notFound } from "./middleware/notFound.middleware";
 import { errorHandler } from "./middleware/errorHandler.middleware";
+import bodyParser from "body-parser";
 
 config();
 
 const app = express();
 
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(cors());
+
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
 app.use("/api/", api);
 
 app.use(notFound);
